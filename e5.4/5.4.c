f3ad469b250358d0bfd8e2a56c2499f1e98834ea
Anderson Carvalho <andersonfelipe01@live.com>
2019-07-29 00:41:54 -0300

    Exercicio numero 18

---
+++ e5.4/5.4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*5. Cadeias de Caracteres. Exercício 5.4 */
+
+/*Todas as letras de uma frase serão a letra seguinte */
+void shift_string( char* str )
+{
+    for( int i = 0; str[i] != '\0'; i++ )
+    {
+        if( str[i] == 'a' )
+        {
+            str[i] = 'z';
+        }
+
+        else if( str[i] == 'A' )
+        {
+            str[i] = 'Z';
+        }
+
+        else if( ( str[i] > 'a' && str[i] < 'z' ) || ( str[i] > 'A' && str[i] < 'Z' ) )
+        {
+            str[i]++;
+        }
+    }
+}
+
+/*Função principal */
+int main( void )
+{
+    char frase[256];
+
+    printf("Digite uma frase: ");
+    scanf(" %255[^\n]", frase);
+
+    shift_string( frase );
+
+    printf("Sua frase ficou:\n\n%s\n\n", frase);
+
+    return 0;
+}
